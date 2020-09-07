1226c4ffe8780c5e64ac9a7ba4ebb1334a04b624
Jenkins <jenkins@regula.by>
2020-09-07 12:33:33 +0300

    0.0.21

--- example/run-android.bat
+++ example/run-android.bat
@@ -1,3 +1,2 @@
-cd "C:\Users\Pavel.Masiuk\StudioProjects\react\DocumentReader"
 call react-native run-android
 exit 0
--- example/start.bat
+++ example/start.bat
@@ -1,4 +1,4 @@
-cd "C:\Users\Pavel.Masiuk\StudioProjects\react\DocumentReader"
+cd "../"
 start cmd.exe @cmd /k "run-android.bat"
 call react-native start
 exit 0
